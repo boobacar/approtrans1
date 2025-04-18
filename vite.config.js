7716d1efc9b046f6a0218bf32641a04fc2f8a5a3
boobacar <boubsfal@gmailcom>
2025-04-18 23:19:47 +0000

    Ready for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/approtrans1/", // 👈 use your repo name here
   plugins: [tailwindcss(), react()],
 });
